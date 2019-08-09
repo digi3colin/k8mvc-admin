0a04312061260e34881373468ecd0cd9722eb918
Colin Leung <colin@digi3studio.com>
2019-08-09 20:56:09 +0800

    update for k8mvc@0.1.14

--- init.js
+++ init.js
@@ -1,8 +1,8 @@
 const K8 = require('k8mvc');
 const RouteList = K8.require('RouteList');
 
-RouteList.add('/admin', 'controller/admin/ControllerAdminHome');
-RouteList.add('/admin/login', 'controller/ControllerAuth', 'login');
-RouteList.add('/admin/login', 'controller/ControllerAuth', 'auth', 'post');
-RouteList.add('/admin/login/fail', 'controller/ControllerAuth', 'fail');
-RouteList.add('/admin/logout', 'controller/ControllerAuth', 'logout');
+RouteList.add('/admin', 'controllers/admin/ControllerAdminHome');
+RouteList.add('/admin/login', 'controllers/ControllerAuth', 'login');
+RouteList.add('/admin/login', 'controllers/ControllerAuth', 'auth', 'post');
+RouteList.add('/admin/login/fail', 'controllers/ControllerAuth', 'fail');
+RouteList.add('/admin/logout', 'controllers/ControllerAuth', 'logout');
