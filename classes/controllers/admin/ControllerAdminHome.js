cfecc494ea25a2f96b7ea7a8dd0c01dd725d33c8
Colin Leung <colin@digi3studio.com>
2019-08-19 22:32:18 +0800

    update Controller Admin Home

--- classes/controllers/admin/ControllerAdminHome.js
+++ classes/controllers/admin/ControllerAdminHome.js
@@ -1,13 +1,10 @@
 const K8 = require('k8mvc');
 const ControllerAdmin = K8.require('ControllerAdmin');
 
-module.exports = class ControllerAdminHome extends ControllerAdmin{
-  constructor(request, response) {
-    super(request, response);
-    this.model = {name: 'Home', lowercase: 'home'};
-  }
-
+class ControllerAdminHome extends ControllerAdmin{
   action_index() {
     this.tpl = this.getView('templates/admin/home', {});
   }
-};
+}
+
+module.exports = ControllerAdminHome;
